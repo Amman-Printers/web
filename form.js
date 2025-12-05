ac310b12e22681a7c524ee14ad67f0ad93ef4688
sathees <satheespalanisamy@gmail.com>
2025-12-05 12:04:23 +0000

    numToWords

--- form.js
+++ form.js
@@ -257,23 +257,28 @@ async function generatePDF() {
     safeGet("at").setText(Math.floor(allAmt).toString());
     safeGet("pat").setText(((allAmt % 1) * 100).toFixed(0).padStart(2, "0"));
 
-    // const toWords = new ToWords({
-    //     localeCode: "en-IN",
-    //     converterOptions: {
-    //         currency: true,
-    //         ignoreDecimal: false,
-    //         ignoreZeroCurrency: false,
-    //         doNotAddOnly: false,
-    //         currencyOptions: {
-    //             name: "Rupee",
-    //             plural: "Rupees",
-    //             symbol: "₹",
-    //             fractionalUnit: { name: "Paisa", plural: "Paise", symbol: "" },
-    //         },
-    //     },
-    // });
-
-    // safeGet("ru").setText(toWords.convert(allAmt.toFixed(2)).toString());
+    const numToWords = (n) => {
+             const a = ['','One ','Two ','Three ','Four ','Five ','Six ','Seven ','Eight ','Nine ','Ten ','Eleven ','Twelve ','Thirteen ','Fourteen ','Fifteen ','Sixteen ','Seventeen ','Eighteen ','Nineteen '];
+             const b = ['', '', 'Twenty','Thirty','Forty','Fifty','Sixty','Seventy','Eighty','Ninety'];
+             const num = parseFloat(n).toFixed(2);
+             const [whole, fraction] = num.split('.');
+             const convert = (num) => {
+                 if ((num = num.toString()).length > 9) return 'overflow';
+                 n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+                 if (!n) return; var str = '';
+                 str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'Crore ' : '';
+                 str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'Lakh ' : '';
+                 str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'Thousand ' : '';
+                 str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'Hundred ' : '';
+                 str += (n[5] != 0) ? ((str != '') ? 'And ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]])  : '';
+                 return str;
+             };
+             let str = convert(whole) + "Rupees ";
+             if (parseInt(fraction) > 0) { str += "And " + convert(fraction) + "Paise "; }
+             return str + "Only";
+        };
+    
+    safeGet("ru").setText(numToWords(allAmt));
 
     try { form.flatten(); } catch (err) {}
 
