d2fd45ac2e8140f52e07736290bf7a9c6b5b7ac5
sathees <satheespalanisamy@gmail.com>
2025-12-07 10:41:17 +0000

    changed config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
     // Replace this with your deployed Google Apps Script Web App URL
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycby0N9qnNUPv7Wo7tKj4SWc6I2AA5P75FVrFtXTXfHwxlXi0wDQ25dTZq4I8pneHAmRnkg/exec',
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxxCBiIdIEWGOCd6P_wa3xONKh6fc7JkPAo9T0bR0NpOCDO4yDq4I_RxdoHgDo10yn_GQ/exec',
     API_TOKEN: 'gfghguytuyghgght6756hgfh67r'
 };
